527e3ea4edaeb4a496c0f7811b7ee34c434ac0f2
Mister_KoRo <35098503+korobot@users.noreply.github.com>
2021-01-03 14:47:09 +0100

    :lipstick: Ameliorate design

--- assets/js/index.js
+++ assets/js/index.js
@@ -7,6 +7,6 @@
 new fullpage('.k-m', {
     navigation: true,
     responsiveWidth: 600,
-    anchors: ['k-m', 'k-i', 'k-pj', 'k-sk'],
+    anchors: ['k-m', 'k-i', 'k-pj', 'k-sk', 'k-contact'],
     licenseKey: 'KGl8O!P%k0'
 })
