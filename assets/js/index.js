5c4651cdf272340d814cc4b4795bde267861d927
Mister_KoRo <35098503+korobot@users.noreply.github.com>
2021-03-17 18:46:59 +0100

    :boom: Change domain name

--- assets/js/index.js
+++ assets/js/index.js
@@ -10,3 +10,12 @@ new fullpage('.k-m', {
     anchors: ['k-m', 'k-i', 'k-pj', 'k-sk', 'k-contact'],
     licenseKey: 'KGl8O!P%k0'
 })
+
+if (localStorage.getItem("hasClosedDomainBanner") && localStorage.getItem("hasClosedDomainBanner") == "true") {
+    document.getElementsByClassName("k-header")[0].classList.add("hidden-full");
+}
+
+document.getElementById("close-btn").onclick = () => {
+    localStorage.setItem("hasClosedDomainBanner", "true");
+    document.getElementsByClassName("k-header")[0].classList.add("hidden");
+}
