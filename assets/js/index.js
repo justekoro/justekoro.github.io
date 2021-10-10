fc939853a28cdb3eeaac15601d62d97af0e7bb45
koro <me@krbk.dev>
2021-10-10 18:50:25 +0200

    :boom: Add light mode

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,12 +1,36 @@
 window.onload = () => {
-    let src=$("#bg").css("background-image");
-    let url=src.match(/\((.*?)\)/)[1].replace(/('|")/g,'');
-    let img=new Image();
-    img.onload=()=>{
-        let el = document.getElementsByClassName("bg-dark")[0];
-        el.classList.add("bg-transparent")
-        el.classList.remove("bg-dark");
+    dme = true;
+    const loadBgIMG = () => {
+        let src=$("#bg").css("background-image");
+        let url=src.match(/\((.*?)\)/)[1].replace(/('|")/g,'');
+        let img=new Image();
+        img.onload=()=>{
+            let el = document.getElementById("ted");
+            el.classList.add("bg-transparent");
+            el.classList.remove("bg-dark");
+        }
+        img.src=url;
+        if(img.complete)img.onload();
     }
-    img.src=url;
-    if(img.complete)img.onload();
+
+    if (!localStorage.getItem("darkModeEnabled") || localStorage.getItem("darkModeEnabled") == "true") {
+        localStorage.setItem("darkModeEnabled",true);
+        console.log("loading");
+        loadBgIMG()
+    } else {
+        dme = false;
+        $("#bg").css("background-image","url(assets/images/background-light.jpg)");
+        $("#ict").text("Turn off the lights");
+        let n = $("#icn");
+        n.removeClass("fa-sun-o");
+        n.addClass("fa-moon-o");
+        console.log("loading");
+        loadBgIMG();
+    }
+
+    $("#light").click(()=>{
+        localStorage.setItem("darkModeEnabled",!dme);
+        location.reload();
+    })
+
 }
