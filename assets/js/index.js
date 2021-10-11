dab4d7b438776d96de11deed34eef5f6ca30ffb1
koro <me@krbk.dev>
2021-10-11 19:41:28 +0200

    :sparkles: Add categories

--- assets/js/index.js
+++ assets/js/index.js
@@ -34,4 +34,46 @@ window.onload = () => {
         location.reload();
     })
 
+    $("#wai-t").click(()=>{
+        $("#presa").addClass("fade-2s");
+        setTimeout(()=>{
+            $("#title").text("Who am I?");
+            $("#subtitle").css("display","none");
+            $("#text").css("display","block");
+            $("#text").html("hi! I'm koro, a 15 yo developer from France.<br/>I started programming in 2017 with Discord Bots, applications that allows you to use almost the full potential of Discord by coding. Then, I wanted to try other things and I got interested in web development in 2019. I only have the basics in frontend development.");
+            $(".active")[0].classList.remove("active")
+            $("#wai-t").addClass("active");
+            if(!$("#arrows").hasClass("invisible"))$("#arrows").addClass("invisible");
+            setTimeout(()=>$("#presa").removeClass("fade-2s"),1000)
+        },1000);
+    });
+
+    $("#home").click(()=>{
+        $("#presa").addClass("fade-2s");
+        setTimeout(()=>{
+            $("#title").text("krbk");
+            $("#subtitle").text("Backend developer")
+            $("#subtitle").css("display","block");
+            $("#text").css("display","none");
+            $(".active")[0].classList.remove("active")
+            $("#home").addClass("active");
+            if(!$("#arrows").hasClass("invisible"))$("#arrows").addClass("invisible");
+            setTimeout(()=>$("#presa").removeClass("fade-2s"),1000)
+        },1000);
+    });
+
+    $("#contact").click(()=>{
+        $("#presa").addClass("fade-2s");
+        setTimeout(()=>{
+            $("#title").text("Contact me");
+            $("#text").text("Click on the buttons below!")
+            $("#subtitle").css("display","none");
+            $("#text").css("display","block");
+            if($("#arrows").hasClass("invisible"))$("#arrows").removeClass("invisible");
+            $(".active")[0].classList.remove("active")
+            $("#contact").addClass("active");
+            setTimeout(()=>$("#presa").removeClass("fade-2s"),1000)
+        },1000);
+    })
+
 }
